31856efe9c4ae725df33d8c1f275d8c7609357dc
tjfdk <tjfdk6201@gmail.com>
2022-09-19 00:05:38 +0900

    사운드, 마우스 감도 슬라이더 위치 조정 + 설정 판넬(esc)

---
+++ Assets/01.Script/4.SeolAh/UI/Setting.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.Design.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    [SerializeField] private Image settingPanel = null;
+    private bool isSetting = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isSetting = !isSetting;
+            settingPanel.gameObject.SetActive(isSetting);
+        }
+    }
+}
