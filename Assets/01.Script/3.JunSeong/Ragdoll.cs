2612fa3bf11dac97376f64b466c22c2b939653a9
junsung <forest2847@gmail.com>
2022-09-21 20:23:53 +0900

    레그돌

---
+++ Assets/01.Script/3.JunSeong/Ragdoll.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField] private GameObject originObj;
+    [SerializeField] private GameObject ragdollObj;
+
+    //ragdollObj의 Rigidbody
+    [SerializeField] private Rigidbody head;
+    [SerializeField] private Rigidbody leftArm;
+    [SerializeField] private Rigidbody rightArm;
+    [SerializeField] private Rigidbody leftKnee;
+    [SerializeField] private Rigidbody rightKnee;
+    [SerializeField] private Rigidbody ribs;
+    [SerializeField] private Rigidbody hip;
+
+
+    private void Update() 
+    {
+        //임시 코드
+        if(Input.GetButtonDown("Jump"))
+        {
+            ChangeRagdoll();
+            CopyTransformOriginToRagdoll(originObj.transform, ragdollObj.transform);
+            ribs.AddForce(new Vector3(0, 300f, -300f), ForceMode.Impulse);
+        }
+    }
+
+    private void ChangeRagdoll()
+    {
+        originObj.SetActive(false);
+        ragdollObj.SetActive(true);
+    }
+
+    private void CopyTransformOriginToRagdoll(Transform origin, Transform ragdoll)
+    {
+        for(int i = 0; i < origin.childCount; i++)
+        {
+            if(origin.childCount != 0)
+            {
+                CopyTransformOriginToRagdoll(origin.GetChild(i), ragdoll.GetChild(i));
+            }
+
+            ragdoll.GetChild(i).localPosition = origin.GetChild(i).localPosition;
+            ragdoll.GetChild(i).localRotation = origin.GetChild(i).localRotation;
+        }
+    }
+}
