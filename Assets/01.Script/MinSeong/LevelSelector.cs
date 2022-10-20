f9fd48bc18d5d6b896afcb8ad6eeb1618de99a83
9mean3 <gminseong114@gmail.com>
2022-10-20 20:25:37 +0900

    아.

--- Assets/01.Script/MinSeong/LevelSelector.cs
+++ Assets/01.Script/MinSeong/LevelSelector.cs
@@ -1,25 +1,49 @@
+using MoreMountains.Tools;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+/*using UnityEngine.*/
 
 public class LevelSelector : MonoBehaviour
 {
+    [Header("OpenScene()에 쓰이는 것")]
+    public string SceneName;
+
+    [Header("OpenLevel()에 쓰이는 것")]
     public int level;
+    public bool isOpenLevel;
 
-    Text text;
-
+    TextMeshProUGUI text;   
     private void Start()
     {
-        text = GetComponentInChildren<Text>();
-
-        text.text = level.ToString();
+        if (isOpenLevel)
+        {
+            text = GetComponentInChildren<TextMeshProUGUI>();
+            text.text = level.ToString();
+        }
     }
 
     public void OpenScene()
     {
+        if(gameObject.name == "Start")
+        {
+            SceneLoader.LoadScene("Level Selector");
+        }
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void OpenLevel()
+    {
         SceneManager.LoadScene("Level " + level.ToString());
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
