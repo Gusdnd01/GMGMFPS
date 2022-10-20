7ef3cf74dd822a0957b1caf428200721f2f50e97
damin06 <ko060614@gmail.com>
2022-10-20 20:25:48 +0900

    gun!

---
+++ Assets/01.Script/5.DaMin/GunSetting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GunSetting : ScriptableObject
+{
+    [Header("Gun stats")]
+    [SerializeField] private int damage;        //데미지
+    public int Damage => damage;
+    [SerializeField] private float timeBetweenShooting;     //연사 속도
+    public float TimeBetweenShooting => timeBetweenShooting;
+    [SerializeField] private float spread;      //탄퍼짐
+    public float Spread => spread;
+    [SerializeField] private float range;       //사정거리
+    public float Range => range;
+    [SerializeField] private float reloadTime;      //장전시간
+    public float ReloadTime => reloadTime;
+    [SerializeField] private float timeBetweenShots;        //점사총 연사 간격 (0 초과시 && !allowButtonHold) 
+    public float TimeBetweenShots => timeBetweenShots;
+    [SerializeField] private int magazineSize;      //탄창크기
+    public int MagazineSize => magazineSize;
+    [SerializeField] private int bulletsPerTap;     //한번에 발사되는 총알 수
+    public int BulletsPerTap => bulletsPerTap;
+    [SerializeField] private bool allowButtonHold;      //True = 연사  False = 단발     if(timeBetweenShots > 0 && !allowButtonHold)점사
+    public bool AllowButtonHold => allowButtonHold;
+}
+
---
+++ Assets/01.Script/5.DaMin/GunSystem.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GunSystem : MonoBehaviour
+{
+    [SerializeField] private GunSetting gunSet;
+    // [Header("Gun stats")]
+    // [SerializeField] private int damage;
+    // [SerializeField] private float timeBetweenShooting;
+    // [SerializeField] private float spread;
+    // [SerializeField] private float range;
+    // [SerializeField] private float reloadTime;
+    // [SerializeField] private float timeBetweenShots;
+    // [SerializeField] private int magazineSize;
+    // [SerializeField] private int bulletsPerTap;
+    //[SerializeField] private bool allowButtonHold;
+    private int curbullet;
+    private int bulletsShot;
+
+    //bools 
+    private bool shooting;
+    private bool readyToShoot;
+    private bool reloading;
+
+
+    [Header("Reference")]
+    [SerializeField] private Camera fpsCam;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private RaycastHit rayHit;
+    [SerializeField] private LayerMask Tag;
+
+    [Header("Graphics")]
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private GameObject bulletHoleGraphic;
+
+
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        curbullet = gunSet.MagazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+
+        //SetText
+        text.SetText(curbullet + " / " + gunSet.MagazineSize);
+    }
+    private void MyInput()
+    {
+        if (gunSet.AllowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && curbullet < gunSet.MagazineSize && !reloading) Reload();
+
+        //Shoot
+        if (readyToShoot && shooting && !reloading && curbullet > 0)
+        {
+            bulletsShot = gunSet.BulletsPerTap;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //Spread
+        float x = Random.Range(-gunSet.Spread, gunSet.Spread);
+        float y = Random.Range(-gunSet.Spread, gunSet.Spread);
+
+        //Calculate Direction with Spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        //RayCast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, gunSet.Range, Tag))
+        {
+            Debug.Log(rayHit.collider.name);
+            Debug.DrawRay(fpsCam.transform.position, direction * gunSet.Range, Color.red);
+
+            // if (rayHit.collider.CompareTag("Enemy"))
+            // {
+            //     //rayHit.collider.GetComponent<enemy>().TakeDamage(damage);
+            // }
+
+            // if (rayHit.collider.CompareTag("Player"))
+            // {
+            //     Debug.Log("굿");
+            // }
+        }
+
+
+        //Graphics
+        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.Euler(0, 270, 0));
+
+        curbullet--;
+        bulletsShot--;
+
+        Invoke("ResetShot", gunSet.TimeBetweenShooting);
+
+        if (bulletsShot > 0 && curbullet > 0)
+            Invoke("Shoot", gunSet.TimeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", gunSet.ReloadTime);
+    }
+    private void ReloadFinished()
+    {
+        curbullet = gunSet.MagazineSize;
+        reloading = false;
+    }
+}
