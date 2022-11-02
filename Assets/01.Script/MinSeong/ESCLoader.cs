c5d3df0e8dcacbfaa4b18f54c5e8bb5631367b57
9mean3 <gminseong114@gmail.com>
2022-11-02 19:09:07 +0900

    ss

--- Assets/01.Script/MinSeong/ESCLoader.cs
+++ Assets/01.Script/MinSeong/ESCLoader.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class ESCLoader : MonoBehaviour
 {
-    [SerializeField] GameObject panel;
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject settingPanel;
 
     [SerializeField] Button resumeButton;
 
-    [SerializeField] GameObject isPausePos;
-    [SerializeField] GameObject isResumePos;
+    [SerializeField] GameObject OnePos;
+    [SerializeField] GameObject TwoPos;
+    [SerializeField] GameObject ThreePos;
+    [SerializeField] GameObject FourPos;
+
+    [SerializeField]
+    AudioMixer audioMixer;
+
+    [SerializeField]
+    Slider masterAudioSlider;
+    [SerializeField]
+    Slider FXAudioSlider;
+    [SerializeField]
+    Slider BGMAudioSlider;
 
     public bool isPausing = false;
+    public bool isInSetting = false;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseMenu();
         }
-
+        audioMixer.SetFloat("Master", masterAudioSlider.value);
+        audioMixer.SetFloat("BGM", FXAudioSlider.value);
+        audioMixer.SetFloat("SFX", BGMAudioSlider.value);
         OnPanel();
     }
     
     public void PauseMenu()
     {
         isPausing = !isPausing;
+    }
+
+    public void SettingMenu()
+    {
+        isInSetting = !isInSetting;
     }
 
     public void OtherButton(string sceneName)
@@ -35,17 +57,28 @@ public class ESCLoader : MonoBehaviour
 
     private void OnPanel()
     {
-        if (isPausing)
+        if(isPausing && !isInSetting)
         {
-            panel.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(panel.GetComponent<RectTransform>().anchoredPosition,
-                new Vector2(isPausePos.GetComponent<RectTransform>().anchoredPosition.x,
-                isPausePos.GetComponent<RectTransform>().anchoredPosition.y), 0.1f);
+            LerpMove(pausePanel, TwoPos);
+            LerpMove(settingPanel, ThreePos);
         }
-        else if (!isPausing)
+        if(isPausing && isInSetting)
         {
-            panel.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(panel.GetComponent<RectTransform>().anchoredPosition,
-                new Vector2(isResumePos.GetComponent<RectTransform>().anchoredPosition.x,
-                isResumePos.GetComponent<RectTransform>().anchoredPosition.y), 0.1f);
+            LerpMove(pausePanel, OnePos);
+            LerpMove(settingPanel, TwoPos);
         }
+        if (!isPausing)
+        {
+            LerpMove(pausePanel, ThreePos);
+            LerpMove(settingPanel, FourPos);
+            isInSetting = false;
+        }
+    }
+
+    private void LerpMove(GameObject obj, GameObject pos)
+    {
+        obj.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(obj.GetComponent<RectTransform>().anchoredPosition,
+            new Vector2(pos.GetComponent<RectTransform>().anchoredPosition.x,
+            pos.GetComponent<RectTransform>().anchoredPosition.y), 0.1f);
     }
 }
