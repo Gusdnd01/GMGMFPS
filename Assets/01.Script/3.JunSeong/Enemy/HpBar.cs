0cb60d789b975497fd2ddc571027e84a132b0705
junsung <forest2847@gmail.com>
2022-12-07 18:33:59 +0900

    여우 모델 추가

--- Assets/01.Script/3.JunSeong/Enemy/HpBar.cs
+++ Assets/01.Script/3.JunSeong/Enemy/HpBar.cs
@@ -38,9 +38,6 @@ public class HpBar : MonoBehaviour
     private void Scale()
     {
         float value = enemy.Health / enemy.MaxHealth * originXScale;
-        Debug.Log(enemy.Health);
-        Debug.Log(enemy.MaxHealth);
-        Debug.Log(value);
 
         hpBar.localScale = new Vector3(value, hpBar.localScale.y, hpBar.localScale.z);
         hpBar.localPosition = new Vector3((originXScale - value) * 0.5f, hpBar.localPosition.y, hpBar.localPosition.z);
