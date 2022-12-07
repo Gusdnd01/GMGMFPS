86bc4242ef65e5c28a0301f798d041bad256598e
Gusdnd01 <cestop42@naver.com>
2022-12-07 17:55:02 +0900

    tbdnt

--- Assets/01.Script/4.SeolAh/UI/Hp.cs
+++ Assets/01.Script/4.SeolAh/UI/Hp.cs
@@ -3,21 +3,24 @@ using UnityEngine.UI;
 
 public class Hp : MonoBehaviour
 {
-    [SerializeField] private PlayerData playerData = null;
+    [SerializeField] private PlayerData playerData;
     [SerializeField] private PlayerController playerC = null;
     [SerializeField] private Slider hpSlider = null;
 
+    float hp;
+    float damage = 10f;
+    float curHp = 100f;
     private void Awake() {
         hpSlider.value = playerData.hp;
+        hp = playerData.hp;
     }
     private void Update() {
-        float tlqkf = 10f;
-        float tleos = 100f;
+        
         if (Input.GetKeyDown(KeyCode.E)) {
             // hpSlider.value -= playerC.currentHp;
-            tleos =- tlqkf;
-            Debug.Log(tleos);
-            hpSlider.value = Mathf.Lerp(tlqkf, tleos, 3f);
+            curHp -= damage;
+            Debug.Log(curHp);
+            hpSlider.value = Mathf.Lerp(hp, curHp, 0.1f);
         }
     }
 }
